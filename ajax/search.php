a8abdfd59ea09ff2cb18ab2c8dba0aec89f515dc
saad <ms22458881@gmail.com>
2023-10-31 10:01:58 +0500

    added

---
+++ ajax/search.php
@@ -0,0 +1,22 @@
+<?php
+include("config.php");
+
+$input=$_POST['input'];
+
+// $select="SELECT *FROM `user` where `name` like '%%'";
+$result=mysqli_query($connection,"SELECT *FROM `user` where `name` like '%$input%' ");
+
+if ($result) {
+    if(mysqli_num_rows($result) > 0){
+        while($data = mysqli_fetch_assoc($result)){
+            echo '<tr>
+            <th scope="row">'.$data['id'].'</th>
+            <td>'.$data['name'].'</td>
+            <td>'.$data['email'].'</td>
+            <td>'.$data['password'].'</td>
+            </tr>';
+        }  
+    }
+}
+
+?>
